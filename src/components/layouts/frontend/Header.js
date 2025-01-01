370332cbf760c2519ff0815e7933c0f18bb3d4ad
razon22 <readul@bdtask.net>
2025-01-02 00:20:56 +0600

    home page responsive

--- src/components/layouts/frontend/Header.js
+++ src/components/layouts/frontend/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CiSearch } from "react-icons/ci";
+import { BiMenuAltRight } from "react-icons/bi";
 const Header = () => {
   return (
     <div className="relative">
@@ -7,32 +7,75 @@ const Header = () => {
       <header className="w-full h-[100px] fixed top-0 left-0 z-20 bg-[#000000bf]  border-b border-[#2C2D2D] text-white flex items-center px-[25px]">
         {/* Logo */}
         <div className="w-[250px] border-r border-[#2C2D2D] h-full flex items-center justify-center">
-          <img
-            src="/images/frontend/logo.png"
-            alt="Logo"
-            className="w-[212px] h-[50px]"
-          />
+          <a href="/">
+            <img
+              src="/images/frontend/logo.png"
+              alt="Logo"
+              className="w-[140px] lg:w-[212px] h-[33px] lg:h-[50px]"
+            />
+          </a>
         </div>
 
         {/* Menu */}
-        <div className="flex justify-between items-center px-[25px] flex-1">
-          <ul className="flex gap-4 items-center">
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Membership</li>
-            <li>Blog</li>
-            <li>Projects</li>
-            <li>Contact</li>
-            <li>Shop</li>
+        <div className="flex justify-end md:justify-between items-center px-[25px] flex-1 ">
+          <div className="md:hidden">
+            <BiMenuAltRight className="text-white text-[26px] cursor-pointer" />
+          </div>
+          <ul className="md:flex gap-4 items-center hidden">
+            <li>
+              {" "}
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Home
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="" className="text-[13px] lg:text-[16px]">
+                About
+              </a>{" "}
+            </li>
+            <li>
+              <a href="/services" className="text-[13px] lg:text-[16px]">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Membership
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="" className="text-[13px] lg:text-[16px]">
+                Shop
+              </a>
+            </li>
           </ul>
-          <div>
-            <img src="/images/frontend/search.png" alt="Search" />
+          <div className="hidden xl:block">
+            <img
+              src="/images/frontend/search.png"
+              className="cursor-pointer"
+              alt="Search"
+            />
           </div>
         </div>
 
         {/* Contact */}
-        <div className="w-[360px] flex justify-center items-center gap-3 border-l border-[#2C2D2D] h-full">
+        <div className="w-[360px] xl:flex justify-center items-center gap-3 border-l border-[#2C2D2D] h-full hidden">
           <img src="/images/frontend/phone.png" alt="Phone" />
           <div className="flex flex-col">
             <span className="text-[#79BB51]">Call Anytime</span>
@@ -47,14 +90,18 @@ const Header = () => {
         <div className="absolute inset-0 bg-black bg-opacity-45"></div>
 
         {/* Hero Content */}
-        <div className="relative z-10 text-white  items-center justify-center h-full flex flex-col gap-10">
-          <h1 className="text-[95px] leading-[108px] font-bold text-center">
-            Your reliable expert for every <br /> property care need
+        <div className="relative z-10 text-white flex flex-col items-center justify-center h-full gap-6 px-4 md:gap-10">
+          <h1 className="text-[50px] leading-[55px] font-bold text-center md:text-[60px]  md:leading-[70px] lg:text-[80px] lg:leading-[90px] xl:text-[95px] xl:leading-[108px]">
+            Your reliable expert for every <br className="hidden md:block" />{" "}
+            property care need
           </h1>
 
-          <button className="px-12 py-5 bg-[#87C03D] text-[#0F0F0F] text-[14px] font-bold hover:bg-[#0F0F0F] transition-all hover:text-white">
+          <a
+            href="/services"
+            className="px-8 py-4 bg-[#87C03D] text-[#0F0F0F] text-[14px] font-bold hover:bg-[#0F0F0F] transition-all hover:text-white md:px-10 md:py-5 lg:px-12 lg:py-6"
+          >
             Book A Service
-          </button>
+          </a>
         </div>
       </section>
     </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,25 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1980px",
+      // => @media (min-width: 1980px) { ... }
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -16,6 +35,9 @@ export default {
         "testimonial-bg": "url('/images/frontend/home/wave.png')",
         "qoute-bg": "url('/images/frontend/home/qoute-bg.png')",
         "contact-bg": "url('/images/frontend/home/contact-bg.png')",
+      },
+      animation: {
+        "spin-fast": "spin 0.5s linear infinite",
       },
     },
   },
